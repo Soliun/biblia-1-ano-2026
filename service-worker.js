5d95cc14c86501fc155a55175c8519c1a6ed2494
Josias Silva <onofre36110@gmail.com>
2026-01-05 14:45:59 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,43 @@
-const CACHE_NAME = 'biblia-2026-v6';
-const urlsToCache = ['/', '/index.html', '/manifest.json'];
+const CACHE_NAME = 'biblia-1-ano-2026-v1';
+
+const BASE_PATH = '/biblia-1-ano-2026/';
+
+const urlsToCache = [
+  BASE_PATH,
+  BASE_PATH + 'index.html',
+  BASE_PATH + 'manifest.json'
+];
 
 self.addEventListener('install', event => {
   self.skipWaiting();
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
 });
 
 self.addEventListener('activate', event => {
   self.clients.claim();
   event.waitUntil(
-    caches.keys().then(keys => 
-      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
     )
   );
 });
 
 self.addEventListener('fetch', event => {
+  const url = new URL(event.request.url);
+
+  if (!url.pathname.startsWith(BASE_PATH)) return;
+
   event.respondWith(
-    caches.match(event.request).then(response => 
-      response || fetch(event.request).catch(() => caches.match('/index.html'))
-    )
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request).catch(() =>
+        caches.match(BASE_PATH + 'index.html')
+      );
+    })
   );
 });
