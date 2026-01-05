2100d8be49f2c6e5d2911f9e1ae636532c6e6a12
Josias Silva <onofre36110@gmail.com>
2026-01-05 13:53:33 -0300

    Add files via upload
    Primeira versão do app

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'biblia-2026-v6';
+const urlsToCache = ['/', '/index.html', '/manifest.json'];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+});
+
+self.addEventListener('activate', event => {
+  self.clients.claim();
+  event.waitUntil(
+    caches.keys().then(keys => 
+      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
+    )
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => 
+      response || fetch(event.request).catch(() => caches.match('/index.html'))
+    )
+  );
+});
